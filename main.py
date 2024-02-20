0f072920e9e88a0313018b9035c66ba350b59bcd
Hussain Erfani <145542696+erfani-hussain@users.noreply.github.com>
2024-02-20 17:07:14 +0100

    main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from game_data import data
 import random
 from art import logo, vs
-# from replit import clear
+from replit import clear
 from os import system
 
 def get_random_account():
@@ -49,7 +49,12 @@ def game():
     b_follower_count = account_b["follower_count"]
     is_correct = check_answer(guess, a_follower_count, b_follower_count)
 
-    system('clear')
+    # NOTE:
+    # if you're on Windows or Linux try this to clear the screen 
+    # system('clear')
+    # but if if you wanna try it on replit, u have to try this instead
+    clear()
+    
     print(logo)
     if is_correct:
       score += 1
@@ -67,27 +72,3 @@ FAQ: Why does choice B always become choice A in every round, even when A had mo
 Suppose you just started the game and you are comparing the followers of A - Instagram (364k) to B - Selena Gomez (174k). Instagram has more followers, so choice A is correct. However, the subsequent comparison should be between Selena Gomez (the new A) and someone else. The reason is that everything in our list has fewer followers than Instagram. If we were to keep Instagram as part of the comparison (as choice A) then Instagram would stay there for the rest of the game. This would be quite boring. By swapping choice B for A each round, we avoid a situation where the number of followers of choice A keeps going up over the course of the game. Hope that makes sense :-)
 
 '''
-
-
-
-# Generate a random account from the game data.
-
-# Format account data into printable format.
-
-# Ask user for a guess.
-
-# Check if user is correct.
-## Get follower count.
-## If Statement
-
-# Feedback.
-
-# Score Keeping.
-
-# Make game repeatable.
-
-# Make B become the next A.
-
-# Add art.
-
-# Clear screen between rounds.
